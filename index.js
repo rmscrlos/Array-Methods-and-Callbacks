d5a305edb8eddb598068d0856179aff9658acf59
Jean Carlos  Ramos <jeancarlosramos@Jeans-MacBook-Pro.local>
2020-09-04 12:33:25 -0400

    completed task 2

--- index.js
+++ index.js
@@ -42,11 +42,13 @@ if(home > away){
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
+function getFinals(array) {
 
-    /* code here */
+    return array.filter(array => array.Stage === "Final");
 
 };
+
+console.log(getFinals(fifaData));
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
